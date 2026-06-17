800f7bbc49ed2d42725a41aeeb630504577d1c9e
Jesse Abdul <jesse.abdul@noaa.gov>
2026-06-17 11:37:34 -1000

    Updated project_runtime_config.sh to remove the branch argument so it will clone the default branch

--- projects/CTP/config/project_runtime_config.sh
+++ projects/CTP/config/project_runtime_config.sh
@@ -12,4 +12,4 @@
 ##### Project Configuration Variables: #####
 
 	# define the container git project URL
-	GIT_URL="--branch Branch_CODE_v1.4_install git@github.com:noaa-pifsc/PIFSC-CTP-Containerized-Oracle-Development-Environment.git"
+	GIT_URL="git@github.com:noaa-pifsc/PIFSC-CTP-Containerized-Oracle-Development-Environment.git"
